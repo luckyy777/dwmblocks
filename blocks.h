50fc8d86e3957320180900540a8eb56c57fddf83
hispanicsanic <76631426+hispanicsanic@users.noreply.github.com>
2021-12-04 12:46:18 +0000

    Add files via upload

--- blocks.h
+++ blocks.h
@@ -4,8 +4,6 @@ static const Block blocks[] = {
 
 	{"", "bash ~/.local/bin/statusbar/music.sh", 1, 20},
 
-	{"", "bash ~/.local/bin/statusbar/countdown.sh", 60, 0},
-
 	{"", "bash ~/.local/bin/statusbar/temp.sh", 5, 0},
 
 	{"", "bash ~/.local/bin/statusbar/ram.sh", 5, 0},
